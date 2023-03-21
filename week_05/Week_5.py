13f03e32f52406c8830c1df9ff9b38823f64c20d
edsaa <esaavedrac@u.northwestern.edu>
2023-03-21 15:32:30 -0500

    update layout

--- week_05/Week_5.py
+++ week_05/Week_5.py
@@ -1,45 +1,58 @@
 import streamlit as st
-import plotly.graph_objects as go
-import numpy as np
-from itertools import cycle
+import pickle
 
-st.set_page_config(layout='wide')
-
-with open("assets/style.css") as f:
-    st.markdown(f"<style> {f.read()} </style>", unsafe_allow_html=True)
-
-#####################################################################
-
-st.title("CIV-ENV 340: Hydraulics and hydrology")
-"****"
-
-with st.sidebar:
-    lottie = """
-    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-    <lottie-player src="https://assets4.lottiefiles.com/private_files/lf30_nep75hmm.json"  background="transparent"  speed="1.5"  style="width: 200px; height: 100px;"  loop  autoplay></lottie-player>
-    """
-    st.components.v1.html(lottie, width=200, height=100)
-
-    "## Week 5"
-    "### Select a topic:"
-    option = st.radio("Select a topic:",
-        [],
-        label_visibility="collapsed")
+def main():
     
-    "***"
-    st.image("https://proxy-na.hosted.exlibrisgroup.com/exl_rewrite/syndetics.com/index.php?client=primo&isbn=9780134292380/sc.jpg")
+    with open("assets/page_config.pkl", 'rb') as f:
+        st.session_state.page_config = pickle.load(f)
     
-    r"""
-    #### Class textbook:
-    [🌐](https://search.library.northwestern.edu/permalink/01NWU_INST/h04e76/alma9980502032702441]) *Houghtalen, Akan & Hwang* (2017). **Fundamentals of hydraulic engineering systems** 5th ed.,
-    Pearson Education Inc., Boston.
-    """
+    st.set_page_config(**st.session_state.page_config)
 
-####################################################################
+    with open("assets/style.css") as f:
+        st.markdown(f"<style> {f.read()} </style>", unsafe_allow_html=True)
+
+    #####################################################################
+
+    st.title("CIV-ENV 340: Hydraulics and hydrology")
+    "****"
+
+    with st.sidebar:
+        lottie = """
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+        <lottie-player src="https://assets4.lottiefiles.com/private_files/lf30_nep75hmm.json"  background="transparent"  speed="1.5"  style="width: 200px; height: 100px;"  loop  autoplay></lottie-player>
+        """
+        st.components.v1.html(lottie, width=200, height=100)
+
+        "### Select a topic:"
+        option = st.radio("Select a topic:",
+            [],
+            label_visibility="collapsed")
+        
+        "***"
+        st.image("https://proxy-na.hosted.exlibrisgroup.com/exl_rewrite/syndetics.com/index.php?client=primo&isbn=9780134292380/sc.jpg")
+        
+        r"""
+        #### Class textbook:
+        [🌐](https://search.library.northwestern.edu/permalink/01NWU_INST/h04e76/alma9980502032702441]) *Houghtalen, Akan & Hwang* (2017). **Fundamentals of hydraulic engineering systems** 5th ed.,
+        Pearson Education Inc., Boston.
+        """
     
-if option == "":
-    r" ### 🚧 Under construction 🚧"
+        cols = st.columns(2)
+        with cols[0]:
+            r"""
+            [![Github Repo](https://img.shields.io/static/v1?label=&message=Repository&color=black&logo=github)](https://github.com/edsaac/NU.CIVENV340)
+            """
+        with cols[1]:
+            r""" [![Other stuff](https://img.shields.io/static/v1?label=&message=Other+stuff&color=white&logo=streamlit)](https://edsaac.github.io)"""
+    
+    ####################################################################
+    
+    if option == "":
+        r" ### 🚧 Under construction 🚧"
 
-else: 
-    st.error("You should not be here!")
-    r" ### 🚧 Under construction 🚧"
+    else: 
+        st.error("You should not be here!")
+        r" ### 🚧 Under construction 🚧"
+
+if __name__ == "__main__":
+    main()
